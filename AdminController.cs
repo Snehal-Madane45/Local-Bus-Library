b9a6581a0f3b401f7cf809439ee49f105d3289f0
Snehal-Madane45 <103511153+Snehal-Madane45@users.noreply.github.com>
2022-04-12 13:40:04 +0000

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,66 @@
+﻿using Local_Bus_Library;
+using Local_Bus_Library.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusPassGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        PassContext passContext=new PassContext();
+        //public AdminController(PassContext passContext)
+        //{
+        //    this.passContext = passContext;
+        //}
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(passContext.AdminDetails);
+        }
+        [HttpPost]
+        public IActionResult AddAdminDetails(AdminDetails admin)
+
+        {
+            passContext.AdminDetails.Add(admin);
+            passContext.SaveChanges();
+            return Ok(passContext.AdminDetails);
+        }
+        [HttpPut]
+        public IActionResult UpdateAdminDetails(AdminDetails admin)
+        {
+            var admindetails = passContext.AdminDetails.FirstOrDefault(d => d.AdminId == admin.AdminId);
+
+            if (admindetails != null)
+            {
+                admindetails.Name = admin.Name;
+                admindetails.Email = admin.Email;
+                admindetails.Password = admin.Password;
+                passContext.AdminDetails.Update(admindetails);
+                passContext.SaveChanges();
+                return Ok(admindetails);
+            }
+            else
+                return NotFound($"No Details found for:{admin.AdminId}");
+        }
+        [HttpDelete("{adminid}")]
+        public IActionResult Delete(int adminid)
+        {
+            var admindetails = passContext.AdminDetails.FirstOrDefault(d => d.AdminId == adminid);
+
+            if (admindetails != null)
+            {
+                passContext.AdminDetails.Remove(admindetails);
+                passContext.SaveChanges();
+                return Ok("Deleted SuccessFully!!!");
+            }
+            else
+                return NotFound($"No Details found for:{adminid}");
+        }
+    }
+}
---
+++ PassengerLoginController.cs
@@ -0,0 +1,57 @@
+﻿using BusPassGenerator.Model;
+using Local_Bus_Library;
+using Local_Bus_Library.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusPassGenerator.Controllers
+{
+    [Route("api/passengerlogin")]
+    [ApiController]
+    public class PassengerLoginController : ControllerBase
+    {
+        PassContext passContext = new PassContext();
+        [HttpPost]
+        public IActionResult Post(AuthModel auth)
+        {
+            var u = passContext.PassengerProfile.FirstOrDefault(d => d.AadharId ==auth.Username  && d.Password == auth.Password);
+            if (u != null)
+            {
+                var jwtToken = GenerateToken(u.AadharId);
+                return Ok(jwtToken);
+            }
+            else
+                return Unauthorized("Login Failed");
+        }
+        private string GenerateToken(string username)
+        {
+            string jwtToken = string.Empty;
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Local Bus Pass Generator system for Pune City"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, username));
+            
+            var token = new JwtSecurityToken("localbuspassgenerator.com"
+                                            , "localbuspassgenerator.com"
+                                            , claims
+                                            , expires: DateTime.Now.AddDays(7)
+                                            , signingCredentials: credentials);
+
+
+            jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwtToken;
+
+        }
+    }
+}
