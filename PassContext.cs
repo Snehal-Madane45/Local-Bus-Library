b407bb42e5bf594d973efcc898869819efefe59f
snehalmadane45 <snehalmadane45@gmail.com>
2022-04-12 13:21:34 +0000

    Add project files.

---
+++ PassContext.cs
@@ -0,0 +1,18 @@
+﻿using Local_Bus_Library.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Local_Bus_Library
+{
+    public class PassContext:DbContext
+    {
+        public DbSet<Passenger> PassengerProfile { get; set; }
+        public DbSet<PassDetails> PassDetails { get; set; }
+        public DbSet<AdminDetails> AdminDetails { get; set; }
+        protected override void OnConfiguring
+            (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security = False; Server =.\SQLEXPRESS; DataBase = BusPassGeneratorDb; uid = sa; password = pass@123");
+        }
+    }
+}
